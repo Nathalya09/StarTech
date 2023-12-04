bc73fb60b66cd4ff0d0caff17943bb5f13df8bda
nathalyaLima <nathalyalima31032004@gmail.com>
2023-12-04 14:09:16 -0300

    Finalizado

--- comexpart1.js
+++ comexpart1.js
@@ -1,56 +1,56 @@
 alert(" *** BEM-VINDO AO COMEX ***");
 
-    var cabecalho = " <h1> >> COMEX << </h1> <hr> "
+var cabecalho = " <h1> >> COMEX << </h1> <hr> "
 
 
-    var Titulo1 = "1. Visão do Produto";
-    var Titulo2 = "2. Entidades Importantes";
-    var Titulo3 = "3. Requisitos não funcionais";
-    var Titulo4 = "4. Requisitos funcionais";
+var Titulo1 = "1. Visão do Produto";
+var Titulo2 = "2. Entidades Importantes";
+var Titulo3 = "3. Requisitos não funcionais";
+var Titulo4 = "4. Requisitos funcionais";
 
-    var conteudo1 = " <h2> 1. Visão do Produto </h2> <p> O projeto <strong> Comex </strong> é um sistema de e-commerce marketplace. Seu objetivo é permitir que a sua base de clientes tenha acesso a vários vendedores e, assim, consolidar-se como o shopping eletrônico mais popular da internet.</p>"
-    var conteudo2 = " <h2> 2. Entidades Importantes </h2>" +
-        "<ul>" +
-        "<li> <strong> Produto </strong> </li>" +
-        "<li> <strong> Categoria </strong> </li>" +
-        "<li> <strong> Cliente </strong> </li>" +
-        "<li> <strong> Pedido </strong> </li>" +
-        "<li> <strong> Item De Pedido </strong> </li>" +
-        "</ul>"
-    var conteudo3 = " <h2> 3. Requisitos não funcionais </h2>" +
-        "<ul>" +
-        "<li> <strong> Manutenibilidade </strong></li>" +
-        "</ul>"
-    var conteudo4 = "<h2> 4. Requisitos funcionais </h2>" +
-        "<p> Os requisitos funcionais são classificados em três categorias no Comex:<br><br>" +
-        "<strong> Essencial: </strong> implementação obrigatória para o sistema atingir seu objetivo principal;<br><br>" +
-        "<strong> Importante: </strong> acrescentam muito valor ao negócio;<br><br>" +
-        " <strong> Desejável: </strong> incrementam usabilidade e funcionalidades.<br><br>" +
-        "</p>" +
-        "<hr>"
+var conteudo1 = " <h2> 1. Visão do Produto </h2> <p> O projeto <strong> Comex </strong> é um sistema de e-commerce marketplace. Seu objetivo é permitir que a sua base de clientes tenha acesso a vários vendedores e, assim, consolidar-se como o shopping eletrônico mais popular da internet.</p>"
+var conteudo2 = " <h2> 2. Entidades Importantes </h2>" +
+    "<ul>" +
+    "<li> <strong> Produto </strong> </li>" +
+    "<li> <strong> Categoria </strong> </li>" +
+    "<li> <strong> Cliente </strong> </li>" +
+    "<li> <strong> Pedido </strong> </li>" +
+    "<li> <strong> Item De Pedido </strong> </li>" +
+    "</ul>"
+var conteudo3 = " <h2> 3. Requisitos não funcionais </h2>" +
+    "<ul>" +
+    "<li> <strong> Manutenibilidade </strong></li>" +
+    "</ul>"
+var conteudo4 = "<h2> 4. Requisitos funcionais </h2>" +
+    "<p> Os requisitos funcionais são classificados em três categorias no Comex:<br><br>" +
+    "<strong> Essencial: </strong> implementação obrigatória para o sistema atingir seu objetivo principal;<br><br>" +
+    "<strong> Importante: </strong> acrescentam muito valor ao negócio;<br><br>" +
+    " <strong> Desejável: </strong> incrementam usabilidade e funcionalidades.<br><br>" +
+    "</p>" +
+    "<hr>"
 
-    function mostrarCabecalho(cabecalho) {
-        document.write(cabecalho)
-    }
+function mostrarCabecalho(cabecalho) {
+    document.write(cabecalho)
+}
 
 
-    function mostrarConteudo(conteudo) {
-        document.write(conteudo)
-    }
+function mostrarConteudo(conteudo) {
+    document.write(conteudo)
+}
 
-    function linha() {
-        document.write("<hr>")
-    }
+function linha() {
+    document.write("<hr>")
+}
 
-    function rodape() {
-        document.write( "<h2> .:: <strong> Alura LEVEL UP </strong> ::. </h2>")
-    }
+function rodape() {
+    document.write("<h2> .:: <strong> Alura LEVEL UP </strong> ::. </h2>")
+}
 
-    linha()
+linha()
 
-    mostrarCabecalho(cabecalho);
-    mostrarConteudo(conteudo1);
-    mostrarConteudo(conteudo2);
-    mostrarConteudo(conteudo3);
-    mostrarConteudo(conteudo4);
-    rodape();
+mostrarCabecalho(cabecalho);
+mostrarConteudo(conteudo1);
+mostrarConteudo(conteudo2);
+mostrarConteudo(conteudo3);
+mostrarConteudo(conteudo4);
+rodape();
---
+++ comexpart2.js
@@ -0,0 +1,84 @@
+function cabecalho(titulo) {
+    document.write(titulo)
+}
+
+function align() {
+    document.write("<hr>")
+}
+
+function espaçoLinha() {
+    document.write("<br>")
+}
+
+function subTitle(codigo) {
+    document.write("<h3> - Código : " + codigo + "</h3>")
+}
+
+function listaDeClientes(nome, mes, ano) {
+    document.write(
+        "<ul>" +
+        "<li>Nome : " + nome + "</li> <br>" +
+        "<li>Mês de Nascimento : " + mes + "</li> <br>" +
+        "<li>Ano de Nascimento : " + ano + "</li> <br>" +
+        "</ul>"
+    )
+}
+
+function somaDasIdades() {
+    let soma = idade1 + idade2 + idade3
+    return soma
+}
+
+function mediaDasIdades() {
+    let media = ((idade1 + idade2 + idade3) / 3)
+    return media
+}
+
+function conteudo(content) {
+    document.write(content)
+}
+
+function rodape() {
+    document.write("<h2> .:: <strong> Alura LEVEL UP </strong> ::. </h2>");
+}
+
+let dataAtual = new Date();
+let anoAtual = dataAtual.getFullYear();
+
+var title = "<h1> >> COMEX << </h1>";
+var title2 = "<h2> ::Lista de Clientes:: </h2>";
+var nome1 = "Fulano";
+var nome2 = "Beltrano";
+var nome3 = "Ciclano";
+var mesNascimento1 = "Fevereiro";
+var mesNascimento2 = "Maio";
+var mesNascimento3 = "Novembro";
+var anoNascimento1 = 1980;
+var anoNascimento2 = 1981;
+var anoNascimento3 = 1984;
+
+let idade1 = anoAtual - anoNascimento1;
+let idade2 = anoAtual - anoNascimento2;
+let idade3 = anoAtual - anoNascimento3;
+let somaIdades = somaDasIdades()
+let media = mediaDasIdades()
+
+cabecalho(title);
+cabecalho(title2);
+align();
+subTitle(1);
+listaDeClientes(nome1, mesNascimento1, anoNascimento1);
+align();
+subTitle(2);
+listaDeClientes(nome2, mesNascimento2, anoNascimento2);
+align();
+subTitle(3)
+listaDeClientes(nome3, mesNascimento3, anoNascimento3);
+align();
+conteudo(" *** Estatística *** ");
+espaçoLinha();
+conteudo("A soma das idades é : " + somaIdades);
+espaçoLinha();
+conteudo("A média das idades é : " + media.toFixed(0));
+align();
+rodape();
