68b9319a950674d1a73c69b96c2f7d3293948638
nathalyaLima <nathalyalima31032004@gmail.com>
2024-01-29 17:23:39 -0300

    Finalizado

--- sprint03/exercicio2.js
+++ sprint03/exercicio2.js
@@ -1,133 +1,44 @@
 
+var nomesRegistrados = []
+var contador = 0;
 
+console.log()
 
-// function cabecalho(titulo) {
-//     document.write(titulo)
-// }
+function registrosDeUsuario(){
 
-// function align() {
-//     document.write("<hr>")
-// }
+var nome = document.getElementById("nome").value;
 
-// function espaçoLinha() {
-//     document.write("<br>")
-// }
-
-// function subTitle(codigo) {
-//     document.write("<h3> - Código : " + codigo + "</h3>")
-// }
-
-// function listaDeCategorias(categoria, codigoAdicionado) {
-//     document.write(
-//         "<ul>" +
-//         "<li>Código : " + codigoAdicionado + "</li> <br>" +
-//         "<li>Categoria : " + categoria + "</li> <br>" +
-//         "</ul>")
-// }
-
-// function conteudo(content) {
-//     document.write(content);
-// }
-
-// function rodape() {
-//     document.write("<h2> .:: <strong> Alura LEVEL UP </strong> ::. </h2>");
-// }
-
-// function somaDasIdades() {
-//     return idadesComplementares;
-// }
-
-// function media(somaFinal, numeroDeClientes) {
-//     var finalMedia = somaFinal / numeroDeClientes
-//     return finalMedia
-// }
-
-// function enviar(){
-//     let categoriaAdicionada = document.getElementById("categoria").value
-//     console.log(categoriaAdicionada)
-
-//     listaDeCategorias(categoriaAdicionada, codigo);
-// }
-
-// var codigo = 1
-
-// align();
-// espaçoLinha();
-
-
-
-// var idadesComplementares = 0;
-
-// let anoRecente = parseInt(prompt(" Digite o ano atual : "));
-
-// if (anoRecente <= 0) {
-//     document.write("Você digitou um valor inválido");
-// } else {
-
-//     var quantidadeDeClientes = parseInt(prompt("Quantidade de clientes : "));
-
-//     if (quantidadeDeClientes <= 0) {
-//         document.write("Você digitou um valor inválido");
-
-//     } else {
-
-//         for (let teste = 0; teste < quantidadeDeClientes; teste++) {
-//             var nomeCliente = prompt("Escolha um nome : ");
-//             var mesNiver = prompt("Insira seu mês de nascimento : ");
-//             var anoNiver = parseInt(prompt("Insira agora o seu ano de nascimento : "));
-//             var tempoDeVida;
-//             var vida = anoRecente - anoNiver;
-
-
-//             console.log("Idade cliente " + teste + " - " + vida);
-
-//             if (vida <= 17) {
-
-//                 tempoDeVida = "Adolescente"
-
-//             } else if (vida >= 18 && vida <= 29) {
-
-//                 tempoDeVida = "Jovem"
-
-//             } else if (vida >= 30 && vida <= 59) {
-
-//                 tempoDeVida = "Adulto"
-
-//             } else if (vida >= 60) {
-
-//                 tempoDeVida = "Idoso"
-
-//             }
-//             idadesComplementares = idadesComplementares + vida;
-//             subTitle(1 + teste);
-//             listaDeClientes(nomeCliente, mesNiver, anoNiver, tempoDeVida);
-//             align();
-
-//             var somaIdadesTotal = somaDasIdades()
-//             var mediaIdades = media(somaIdadesTotal, quantidadeDeClientes)
-
-//         }
-
-//         
-
-//         cabecalho(title2);
-//         conteudo(" *** Estatística *** ");
-//         espaçoLinha();
-//         conteudo("A soma das idades é : " + somaIdadesTotal);
-//         espaçoLinha();
-//         conteudo("A média das idades é : " + mediaIdades);
-//         rodape();
-//     }
-// }
-
-
-    var nomesRegistrados = {} 
-    var contador = 0;
-
-    function cadastroUsuario(){
-        var nome = document.getElementById("Nome").value;
-    
-    if(nome.trim()===""){
-        alert("Insira um nome"); }    
-    
+    if (nome.trim() === "") {
+    alert("Por favor, insira um nome");
+    } else {
+        if (nomesRegistrados.some(usuario => usuario.nome === nome)){
+            alert("Nome já registrado!")
+        } else {
+        contador ++;
+        nomesRegistrados.push({ codigo: contador, nome: nome });
+        alert (nome + " Registrado com sucesso!")
     }
+    } 
+    }
+
+function exibirNomes(){
+    var nomesLista = document.getElementById("categorias");
+    nomesLista.innerHTML = ""; 
+
+for (var i = 0 ;i < nomesRegistrados.length; i++){
+    var li = document.createElement("li");
+    var usuario = nomesRegistrados[i];
+    li.appendChild(document.createTextNode(`Código ${usuario.codigo}`));
+    li.appendChild(document.createElement("br"));
+    li.appendChild(document.createTextNode(`Nome: ${usuario.nome}`))
+    li.appendChild(document.createElement("hr"))
+
+    nomesLista.appendChild(li);
+} 
+}
+
+var botaoCriar = document.getElementById("criarButton");
+botaoCriar.onclick = cadastrodeusuario;
+
+var botaoExibirLista = document.getElementById("botaoExibirLista");
+botaoExibirLista.onclick = exibirNomes;
