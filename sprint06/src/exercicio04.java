cbcb29cb3ca9ebc368e7b468e2f03ee89456fb39
nathalyaLima <nathalyalima31032004@gmail.com>
2024-02-05 17:09:13 -0300

    Finalizado

---
+++ sprint06/src/exercicio04.java
@@ -0,0 +1,18 @@
+public class exercicio04 {
+    public static void main(String[] args) {
+        System.out.println(">>> Listagem dos Clientes");
+        String nome = "Fulano";
+        int dataDeNascimento = 2004;
+
+        String nome2 = "Sicrano";
+        int dataDeNascimento2 = 2000;
+
+        System.out.println("________________________________________________");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Data De Nascimento: " + dataDeNascimento);
+        System.out.println("________________________________________________");
+        System.out.println("Nome: " + nome2);
+        System.out.println("Data De Nascimento: " + dataDeNascimento2);
+    }
+}
---
+++ sprint06/src/exercicio05.java
@@ -0,0 +1,20 @@
+public class exercicio05 {
+    public static void main(String[] args) {
+
+        System.out.println(">>> Listagem de Produtos");
+        String nomeLivro = "Livro";
+        Double precoLivro = 49.95;
+
+        String nomeCelular = "Celular";
+        Double precoCelular = 1470.05;
+
+        System.out.println("_____________________________________________________");
+
+        System.out.println("Livro: " + nomeLivro);
+        System.out.println("Preço: R$ "+ precoLivro);
+        System.out.println("_____________________________________________________");
+        System.out.println("Celular: " + nomeCelular);
+        System.out.println("Preço: R$ " + precoCelular);
+
+    }
+}
